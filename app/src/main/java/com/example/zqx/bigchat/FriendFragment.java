1d4dd8f3db624c0d8e8b903252f73c57ef9d1526
Lenovo <1248913361@qq.com>
2017-11-28 15:03:21 +0800

    自定义adapter实现好友列表

--- app/src/main/java/com/example/zqx/bigchat/FriendFragment.java
+++ app/src/main/java/com/example/zqx/bigchat/FriendFragment.java
@@ -5,19 +5,27 @@ package com.example.zqx.bigchat;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 //import android.support.v4.app.ListFragment;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 /**
  * Created by Zqx on 2017/11/25.
  */
 
-public class FriendFragment extends Fragment{
+public class FriendFragment extends ListFragment{
 
-   /* private String[]friend_list={"赵一","钱二","孙三","李四","周五","吴六","郑七","王八"};
-    @SuppressLint("MissingSuperCall")
+   private String[] friend_list={"赵一","钱二","孙三","李四","周五","吴六","郑七","王八","何九","吕十","施百","张千","刘万"};
+
+    private TextView text;
+   /* @SuppressLint("MissingSuperCall")
 
     public void onActivityCreated(Bundle savedInstanceState){
 
@@ -26,8 +34,47 @@ public class FriendFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.friend_fragment,container,false);
+        setListAdapter(new MyAdapter());
         return view;
     }
+
+
+
+    private class MyAdapter extends BaseAdapter{
+
+        @Override
+        //数组长度
+        public int getCount() {
+            return friend_list.length;
+        }
+
+        @Override
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if(convertView == null){
+                view = View.inflate(getActivity(),R.layout.item,null);
+            }else{
+                view = convertView;
+            }
+            text = (TextView) view.findViewById(R.id.item_textview);
+            text.setText(friend_list[position]);
+
+            return view;
+
+        }
+    }
+
 
 }
 
